fd6206e51b61201fd07197a3430a722920d885d2
Jan Bölsche <regular.gonzales@googlemail.com>
2017-09-20 11:51:02 +0200

    Move getConfig from client.js to config.js

--- client.js
+++ client.js
@@ -1,43 +1,6 @@
 const ssbClient = require('ssb-client')
-const ssbKeys = require('ssb-keys')
-const querystring = require('querystring')
 const merge = require('deep-extend')
-
-function parseConfig(str) {
-  try {
-    return JSON.parse(Buffer.from(querystring.parse(str).s, 'base64').toString())
-  } catch(e) {
-    return null
-  }
-}
-
-function getConfig() {
-  let ret
-  const fromEnv = process.env.config_hash
-  ret = parseConfig(fromEnv)
-  if (ret) {
-    ret.keys = ssbKeys.loadOrCreateSync('electroparty-keys')
-    console.log('ep: config from env')
-    console.log(`browser pubkey: ${ret.keys.id}`)
-    return ret
-  }
-
-  const fromUrl = document.location.hash && document.location.hash.slice(1)
-  ret = parseConfig(fromUrl)
-  if (ret) {
-    console.log('ep: config from url')
-    document.location.hash = ''
-    localStorage['electroparty-config'] = fromUrl
-    return ret
-  }
-  const fromStorage = localStorage['electroparty-config']
-  if (fromStorage) {
-    console.log('ep: config from localStorage')
-    ret = parseConfig(fromStorage)
-    return ret
-  }
-  throw new Error('ssb-electroparty did not find config data. D:')
-}
+const getConfig = require('./config.js') // .js is intentional
 
 module.exports = function(opts, cb) {
   if (typeof opts === 'function') {cb = opts; opts = {}}

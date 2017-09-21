a93325f32cb0aa00fb590cae40f081a5ac31fbc2
Jan Bölsche <regular.gonzales@googlemail.com>
2017-09-21 19:10:32 +0200

    Add helper scripts (need to be made generic)

---
+++ onboard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$#" -ne 2 ]; then
+  echo "Usage: configfile \"firstname lastname\""
+  exit 1
+fi
+ls config 2>/dev/null && echo "Please move away the config file" && exit 1
+set -e
+# usage: onboard.sh ../shared/config "Firsname Lastname"
+bin/update-onboarding-config "$1" "$(./getinvite $1)" "$2" > config
+npm run build
+npm run pack
+#rm config
+./upload.sh

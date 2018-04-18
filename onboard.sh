4cde31b12bab7982620dbf20a45aa8e98b987f4b
Jan Bölsche <regular.gonzales@googlemail.com>
2018-04-18 19:31:14 +0200

    Make onboarding scripts project-agnostic

--- onboard.sh
+++ onboard.sh
@@ -6,6 +6,8 @@ fi
 ls config 2>/dev/null && echo "Please move away the config file" && exit 1
 set -e
 # usage: onboard.sh ../shared/config "Firsname Lastname"
+ssb_appname=`node -e "var c = JSON.parse(require('fs').readFileSync('$1')); console.log(c.invite_appName || c.appName)"`
+export ssb_appname
 bin/update-onboarding-config "$1" "$(./getinvite $1)" "$2" > config
 npm run build
 npm run pack
--- upload.sh
+++ upload.sh
@@ -1,5 +1,6 @@
-T="~/dev/lad/dist/"
+T="~/dev/$ssb_appname/dist/"
+ssh korn "mkdir -p $T"
 rsync -avz build/SSB-Electroparty-darwin-x64 "korn:$T"
 R=$(node -e "console.log(require('crypto').randomBytes(10).toString('hex'))")
-ssh korn "cd ~/dev/lad/dist && zip -r /var/www/html/lad-$R.zip SSB-Electroparty-darwin-x64"
-echo "http://pub.postpossessive.org/lad-$R.zip"
+ssh korn "cd ~/dev/$ssb_appname/dist && zip -r /var/www/html/$ssb_appname-$R.zip SSB-Electroparty-darwin-x64"
+echo "http://pub.postpossessive.org/$ssb_appname-$R.zip"
